c84007f9718e07ffb1bacd44f406076e7290d84b
unknown <diebraga0055@gmail.com>
2020-04-24 16:43:00 +0100

    separeted data management in a repository directory

---
+++ src/repositories/AppointmentsRepository.ts
@@ -0,0 +1,42 @@
+import { isEqual } from 'date-fns';
+import Appointment from '../models/Appointment';
+
+// private the var isn't accecible out of the class;
+// 'type' defined;
+class AppointmentsRepository {
+  private appointments: Appointment[];
+
+  // init the var ;
+  constructor() {
+    this.appointments = [];
+  }
+// make var public order to be used on appointments.routes
+  public findByDate(date: Date): Appointment | null {
+      // this var saves finds appointments returns True, otherwise 'null'
+  const findAppointment = this.appointments.find(appointment =>
+    isEqual(date, appointment.date),
+  );
+    return findAppointment || null;
+  }
+
+  // 'public' can be used out the class;
+  // declared 'types' down bellow;
+  public create(provider: string, date: Date,): Appointment {
+    const appointment = new Appointment(provider, date);
+
+    this.appointments.push(appointment);
+
+    return appointment;
+  }
+}
+
+export default AppointmentsRepository;
+
+/**
+ *  repositories are responsable in manage all data ex: list
+ *  delete create update etc..
+ *  repository will say how it'd be saved.
+ */
+
+
+// export to your appointments.routes
--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -2,21 +2,23 @@ import { Router } from 'express';
 import { startOfHour, parseISO, isEqual } from 'date-fns';
 // defining 'types' from '../models' of values in the vars down bellow
 import Appointment from '../models/Appointment';
-
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+// var saves all types declared of function needed to create appointments.
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
 // attribuating interface declared above from 'models' 'Appointment[]'
-const appointments: Appointment[] = []
+// temporary var save appointments
+// const appointments: Appointment[] = []
 
 appointmentsRouter.post('/', (req, res) => {
   const { provider, date } = req.body;
+
 // date will be formated to the begining of hour ex: 13:00 in this var
 // date will be coerted in New Date() in this var
-  const formatedDate = startOfHour(parseISO(date))
-  // this var saves equal appointments returns True, otherwise 'False'
-  const findAppointmentSameDate = appointments.find(appointment =>
-      isEqual(formatedDate, appointment.date),
-    );
+  const formatedDate = startOfHour(parseISO(date));
+
+  const findAppointmentSameDate = appointmentsRepository.findByDate(formatedDate);
 
     if (findAppointmentSameDate) {
       return res
@@ -24,9 +26,7 @@ appointmentsRouter.post('/', (req, res) => {
       .json({ message: 'Already booked' });
     }
 
-  const appointment = new Appointment(provider, formatedDate);
-
-  appointments.push(appointment);
+    const appointment = appointmentsRepository.create(provider, formatedDate);
 
   return res.json(appointment)
 })
