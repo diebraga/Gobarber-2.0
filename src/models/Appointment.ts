fb429dc8ee575a764c08fa8aff3687b57f92aa2f
unknown <diebraga0055@gmail.com>
2020-04-30 15:03:24 +0100

    changing model

--- src/models/Appointment.ts
+++ src/models/Appointment.ts
@@ -1,21 +1,28 @@
-import { uuid } from 'uuidv4';
+import { Entity, Column, PrimaryColumn } from 'typeorm';
 
+@Entity('appointments')
 class Appointment {
+  @PrimaryColumn('uuid')
   id: string;
 
+  @Column()
   provider: string;
 
+  @Column('time with time zone')
   date: Date;
-
-  // id will be always a {uuid} 'unique universal id'
-  // declaring value types
-  constructor({ provider, date }: Omit<Appointment, 'id'>) {
-    this.id = uuid();
-    this.provider = provider;
-    this.date = date;
-  }
 }
 
 export default Appointment;
+
+/**
+ *  @Entity above the class reppresents that bellow
+ *  belongs to your database table.
+ *
+ *   @* give parametrs to the columns
+ *
+ *  go to your tsconfig.json and uncomment
+ *  "experimentalDecorators": true, && "emitDecoratorMetadata": true,
+ *  disable you puting in off your: "strictPropertyInitialization": false,
+ */
 
 // this model will be exported to ' /appointments.rutes'
