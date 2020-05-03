22a9db347b2959f36f3e13de4059a1116cdc7180
unknown <diebraga0055@gmail.com>
2020-05-03 10:50:56 +0100

    added password hash

--- src/services/CreateUserService.ts
+++ src/services/CreateUserService.ts
@@ -1,4 +1,5 @@
 import { getRepository } from 'typeorm';
+import { hash } from 'bcryptjs';
 import User from '../models/Users';
 
 interface Request {
@@ -19,10 +20,12 @@ export default class CreateUserServicer {
       throw new Error('Email address already being used');
     }
 
+    const PasswordHash = await hash(password, 8);
+
     const user = usersRepository.create({
       name,
       email,
-      password,
+      password: PasswordHash,
     });
 
     await usersRepository.save(user);
