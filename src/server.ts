dba086a7ddf9ccb508b271e94955e9bd49ea0e6a
unknown <diebraga0055@gmail.com>
2020-05-09 17:22:34 +0100

    showing img

--- src/server.ts
+++ src/server.ts
@@ -2,12 +2,15 @@ import 'reflect-metadata';
 
 import express from 'express';
 import routes from './routes';
+import multerConfig from './config/multerConfig';
+
 
 import './database';
 
 const app = express();
 
 app.use(express.json());
+app.use('/files', express.static(multerConfig.directory));
 app.use(routes);
 
 app.listen(3333, () => {
